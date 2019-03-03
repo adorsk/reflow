8c7143d958390559179279df70aea6599fd48f01
adorsk <adorsk>
2019-03-03 09:26:17 -0500

    use drawImage, offscreen canvas for faster rendering

--- src/stories/colorFns/graphFactory.js
+++ src/stories/colorFns/graphFactory.js
@@ -87,89 +87,6 @@ const graphFactory = ({store} = {}) => {
 
   graph.addNodeFromSpec({
     nodeSpec: {
-      id: 'fill',
-      portSpecs: {
-        'inputs': {
-          height: {
-            initialValues: [100],
-            ctx: { getGuiComponent: () => NumberInput }
-          },
-          width: {
-            initialValues: [150],
-            ctx: { getGuiComponent: () => NumberInput }
-          },
-          fillStyle: generateColorPortSpec(),
-        },
-      },
-      tickFn ({node}) {
-        let srcChanged = false
-        const tickFnSrc = node.tickFn.toString()
-        if (node.state.get('tickFnSrc') !== tickFnSrc) {
-          node.state.set('tickFnSrc', tickFnSrc)
-          srcChanged = true
-        }
-        if (!node.hasHotInputs() && !srcChanged) { return }
-        const ctx = node.state.get('canvas').getContext('2d')
-        for (let dimension of ['width', 'height']) {
-          const port = node.getPort('inputs:' + dimension)
-          if (port.isHot()) { 
-            ctx.canvas[dimension] = port.mostRecentValue
-          }
-        }
-        const fillStyle = node.getPort('inputs:fillStyle').mostRecentValue
-        const imgData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height)
-        const ts = {y: 0, x: 0}
-        for (let y = 0; y < imgData.height; y++) {
-          const rowStartIdx = (y * imgData.width * 4)
-          ts.y = y / imgData.height
-          const color = chroma(fillStyle).set(
-            'lch.c',
-            ((ts.y * 100) + 5 * (-1 + Math.random() * 2)),
-          )
-          const rgba = [...color.rgb(), 255]
-          for (let x = 0; x < imgData.width; x++) {
-            const pixelStartIdx = (rowStartIdx + x * 4)
-            ts.x = x / imgData.width
-            for (let i = 0; i < 4; i++) {
-              imgData.data[pixelStartIdx + i] = rgba[i]
-            }
-          }
-        }
-        ctx.putImageData(imgData, 0, 0)
-      },
-      ctx: {
-        didMountFn ({node}) {
-          if (!node.state.get('initialized')) {
-            const canvas = document.createElement('canvas')
-            node.state.set('canvas', canvas)
-            node.state.set('initialized', true)
-          }
-        },
-        getGuiComponent () {
-          class GuiComponent extends React.Component {
-            constructor (props) {
-              super(props)
-              this.rootRef = React.createRef()
-            }
-            render () { return (<div ref={this.rootRef}/>) }
-            componentDidMount () { this.updateCanvas() }
-            componentDidUpdate () { this.updateCanvas() }
-            updateCanvas () {
-              const { node } = this.props
-              const rootEl = this.rootRef.current
-              rootEl.innerHTML = ''
-              const canvas = node.state.get('canvas')
-              if (canvas) { rootEl.appendChild(canvas) }
-            }
-          }
-          return GuiComponent
-        }
-      },
-    }
-  })
-
-  graph.addNodeFromSpec({
-    nodeSpec: {
       id: 'decorator',
       portSpecs: {
         'inputs': {
@@ -215,9 +132,9 @@ const graphFactory = ({store} = {}) => {
               constant: {
                 valueFn: () => {
                   const decorators = {
-                    imgData (decoratee) {
+                    imageData (decoratee) {
                       const { shape } = decoratee
-                      const imgData = new ImageData(
+                      const imageData = new ImageData(
                         shape.bRect.width,
                         shape.bRect.height
                       )
@@ -225,16 +142,16 @@ const graphFactory = ({store} = {}) => {
                         ...(_.times(3, () => (~~(Math.random() * 255)))),
                         255
                       ]
-                      for (let y = 0; y < imgData.height; y++) {
-                        const rowStartIdx = y * imgData.width * 4
-                        for (let x = 0; x < imgData.width; x++) {
+                      for (let y = 0; y < imageData.height; y++) {
+                        const rowStartIdx = y * imageData.width * 4
+                        for (let x = 0; x < imageData.width; x++) {
                           const pixelStartIdx = rowStartIdx + x * 4
                           for (let i = 0; i < 4; i++) {
-                            imgData.data[pixelStartIdx + i] = rgba[i]
+                            imageData.data[pixelStartIdx + i] = rgba[i]
                           }
                         }
                       }
-                      return imgData
+                      return imageData
                     },
                   }
                   return decorators
@@ -306,7 +223,7 @@ const graphFactory = ({store} = {}) => {
       id: 'quilter',
       portSpecs: {
         inputs: { cells: {} },
-        outputs: { imgData: {} },
+        outputs: { canvas: {} },
       },
       tickFn ({node}) {
         let srcChanged = false
@@ -317,6 +234,10 @@ const graphFactory = ({store} = {}) => {
         }
         if (!node.hasHotInputs() && !srcChanged) { return }
         const cells = node.getPort('inputs:cells').mostRecentValue
+        if (! cells) {
+          node.getPort('outputs:canvas').pushValue(undefined)
+          return
+        }
         const bRect = {
           x: 0,
           y: 0,
@@ -336,30 +257,33 @@ const graphFactory = ({store} = {}) => {
         }
         bRect.width = bRect.right - bRect.left
         bRect.height = bRect.top - bRect.bottom
-        const ctx = document.createElement('canvas').getContext('2d')
+        const ctx = new OffscreenCanvas(bRect.width, bRect.height).getContext('2d')
         ctx.canvas.width = bRect.width
         ctx.canvas.height = bRect.height
         for (let cell of cells) {
-          ctx.putImageData(cell.imgData, cell.shape.bRect.x, cell.shape.bRect.y)
+          const ctxForCell = new OffscreenCanvas(
+            cell.shape.bRect.width, cell.shape.bRect.height
+          ).getContext('2d')
+          ctxForCell.putImageData(cell.imageData, 0, 0)
+          ctx.drawImage(ctxForCell.canvas, cell.shape.bRect.x, cell.shape.bRect.y)
         }
-        const imgData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height)
-        node.getPort('outputs:imgData').pushValue(imgData)
+        node.getPort('outputs:canvas').pushValue(ctx.canvas)
       },
     }
   })
 
   graph.addNodeFromSpec({
     nodeSpec: {
-      id: 'displayImgData',
+      id: 'displayImage',
       portSpecs: {
         'inputs': {
-          imgData: {},
+          image: {},
         },
       },
       tickFn ({node}) {
         if (!node.hasHotInputs()) { return }
-        const imgData = node.getPort('inputs:imgData').mostRecentValue
-        node.state.set('imgData', imgData)
+        const image = node.getPort('inputs:image').mostRecentValue
+        node.state.set('image', image)
       },
       ctx: {
         getGuiComponent () {
@@ -375,11 +299,11 @@ const graphFactory = ({store} = {}) => {
               const { node } = this.props
               const ctx = this.canvasRef.current.getContext('2d')
               ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-              const imgData = node.state.get('imgData')
-              if (! imgData) { return }
-              ctx.canvas.width = imgData.width
-              ctx.canvas.height = imgData.height
-              ctx.putImageData(imgData, 0, 0)
+              const image = node.state.get('image')
+              if (! image) { return }
+              ctx.canvas.width = image.width
+              ctx.canvas.height = image.height
+              ctx.drawImage(image, 0, 0)
             }
           }
           return GuiComponent
@@ -404,7 +328,7 @@ const graphFactory = ({store} = {}) => {
     wireSpec: { src: 'merger:array', dest: 'quilter:cells' }
   })
   graph.addWireFromSpec({
-    wireSpec: { src: 'quilter:imgData', dest: 'displayImgData:imgData' }
+    wireSpec: { src: 'quilter:canvas', dest: 'displayImage:image' }
   })
 
   return graph
